ee4dd8bd38cefe31b94edd8b117869c78d3a70cd
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-18 03:28:38 +0530

    all utils file

---
+++ client/src/componet/utils/util.js
@@ -0,0 +1,85 @@
+import { BsFiletypeHtml, BsFiletypeCss, BsBootstrap } from "react-icons/bs";
+import { FaNodeJs } from "react-icons/fa";
+
+import {
+    SiJavascript,
+    SiMui,
+    SiReact,
+    SiTailwindcss,  
+    SiMongodb,
+    SiMysql,
+    SiGithub,
+    SiExpress,
+
+  } from "react-icons/si";
+
+
+export const TechStackList=[
+    {
+        _id:1,
+        name:'HTML',
+        icon: BsFiletypeHtml,
+    
+    },
+    {
+        _id: 2,
+        name: "CSS",
+        icon: BsFiletypeCss,
+      },
+      {
+        _id: 3,
+        name: "Bootstrap",
+        icon: BsBootstrap,
+      },
+      {
+        _id: 4,
+        name: "Javascript",
+        icon: SiJavascript,
+      },
+      {
+        _id: 5,
+        name: "React JS",
+        icon: SiReact,
+      },
+      {
+        _id: 6,
+        name: "React MUI",
+        icon: SiMui,
+      },
+      {
+        _id: 7,
+        name: "Tailwind ",
+        icon: SiTailwindcss,
+      },
+      {
+        _id: 8,
+        name: "Node JS ",
+        icon: FaNodeJs,
+      },
+      {
+        _id: 9,
+        name: "Express JS ",
+        icon: SiExpress,
+      },
+      {
+        _id: 10,
+        name: "Mongodb ",
+        icon: SiMongodb,
+      },
+      {
+        _id: 11,
+        name: "Git / Github ",
+        icon: SiGithub,
+      },
+      {
+        _id: 12,
+        name: "SQL ",
+        icon: SiMysql,
+      },
+
+]
+
+
+
+
+

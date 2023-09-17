6328399b73a334db26d2bcdb5615205fe4c5f6fb
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-18 03:32:29 +0530

    adding layout section to portfolio

---
+++ client/src/componet/Layout/Layout.jsx
@@ -0,0 +1,39 @@
+import React,{useState} from 'react';
+
+import Home from '../pages/Home/Home'
+import {AiOutlineDoubleLeft,AiOutlineDoubleRight} from 'react-icons/ai'
+import './Layout.css'
+import Menu from '../Menu/Menu.jsx';
+
+const Layout=()=> {
+const[toggle,setToggle]=useState(true);
+
+//change Toggle
+const handleToggle=()=>{
+  setToggle(!toggle);
+}
+  return (
+    <div className="sidebar-section">
+    <div className=  { toggle ? "sidebaar-toggle sidebaar":"sidebaar"}>
+      <div className="sidebaar-toggle-icon">
+     
+        <p onClick={handleToggle}> 
+        {
+          toggle ?(<AiOutlineDoubleLeft size={30}/>):(<AiOutlineDoubleRight size={30}/>)
+        }
+        </p>
+
+        </div>
+        <Menu toggle={toggle}></Menu>
+    </div>
+    <div className="container">
+     <Home></Home>
+
+    </div>
+
+   </div>
+
+  )
+}
+
+export default Layout

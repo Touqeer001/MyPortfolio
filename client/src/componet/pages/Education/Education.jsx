d623e4e97dae975fe16ed759d6f44b765d650478
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-19 03:39:17 +0530

    adding education

--- client/src/componet/pages/Education/Education.jsx
+++ client/src/componet/pages/Education/Education.jsx
@@ -17,26 +17,26 @@ const Education = () => {
             className="vertical-timeline-element--work"
             contentStyle={{ background: "white", color: "black" }}
             contentArrowStyle={{ borderRight: "7px solid  white" }}
-            date="2020 - 2022"
+            date="2020 - 2024"
             iconStyle={{ background: "#138781", color: "#fff" }}
             icon={<MdSchool />}
           >
-            <h3 className="vertical-timeline-element-title">MCA</h3>
+            <h3 className="vertical-timeline-element-title">B.E Computer Engineering</h3>
             <h4 className="vertical-timeline-element-subtitle">
-              IIT Mumbai, IN
+            Theem college of Engineering,University Of Mumbai,India
             </h4>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentStyle={{ background: "white", color: "black" }}
             contentArrowStyle={{ borderRight: "7px solid  white" }}
-            date="2017 - 2020"
+            date="2017 -2018"
             iconStyle={{ background: "#138781", color: "#fff" }}
             icon={<MdSchool />}
           >
-            <h3 className="vertical-timeline-element-title">BCA</h3>
+            <h3 className="vertical-timeline-element-title">Higher Secondary School</h3>
             <h4 className="vertical-timeline-element-subtitle">
-              IIT Mumbai, IN
+              MUmbai,India.
             </h4>
           </VerticalTimelineElement>
         </VerticalTimeline>

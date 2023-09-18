51bf5f189453c46ee1248d40e41d7616a8732a92
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-19 03:41:06 +0530

    profile photo added

---
+++ client/src/componet/context/ThemeContextt.js
@@ -0,0 +1,17 @@
+import React,{useState,createContext,useContext} from 'react';
+
+const ThemContext=createContext();
+
+const ThemProvider=({children})=>{
+    const[theme,setTheme]=useState('light');
+    return(
+    <ThemContext.Provider value={[theme,setTheme]}>
+            {children}
+        </ThemContext.Provider>
+    )
+
+}
+
+//custom Hook
+const useTheme=()=>useContext(ThemContext);
+export  {useTheme,ThemProvider};

9c1fbd48fd383e9781f64a60f4505773547c0493
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-19 03:41:48 +0530

    adding contaxt to index

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ThemProvider} from './componet/context/ThemeContextt.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ThemProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </ThemProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function

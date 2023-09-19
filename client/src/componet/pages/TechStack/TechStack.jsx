b72a6521699ef0918d7520e9b3d05083adb10a5a
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-19 21:24:30 +0530

    addding new css properties

--- client/src/componet/pages/TechStack/TechStack.jsx
+++ client/src/componet/pages/TechStack/TechStack.jsx
@@ -1,44 +1,47 @@
-import React from 'react'
-import './TechStack.css'
-import { TechStackList } from '../../utils/util';
-const TechStack =()=> {
+import React from "react";
+import "./TechStack.css";
+import { TechStackList } from "../../utils/util";
+import Pulse from "react-reveal/Pulse";
+import Fade from 'react-reveal/Fade';
+const TechStack = () => {
   return (
     <>
-    <div className="container techstack" id="techstack">
-    <h2 className="col-12 mt-3 mb-1 text-center text-uppercase">Technologies Stack</h2>
-    <hr></hr>
-    <p className="pb-3 text-center">
+      <div className="container techstack" id="techstack">
+        <Pulse>
+          <h2 className="col-12 mt-3 mb-1 text-center text-uppercase">
+            Technologies Stack
+          </h2>
+          <hr></hr>
+          <p className="pb-3 text-center">
             👉 including programming Languages, frameworks, databses, front-end
             and back-end tools, and APIs
           </p>
-          <div className="row">
-            {TechStackList.map(tech=>(
-           <div key={tech._id} className="col-md-3">
-            <div className="card m-2">
-              <div className="card-content">
-                <div className="card-body">
-                <div className="media d-flex justify-content-center">
-                <div className="alig-self-center">
-                  <tech.icon className='tech-icon'/>
+        </Pulse>
+        <div className="row">
+          {TechStackList.map((tech) => (
+            <Fade left>
+              <div key={tech._id} className="col-md-3">
+                <div className="card m-2">
+                  <div className="card-content">
+                    <div className="card-body">
+                      <div className="media d-flex justify-content-center">
+                        <div className="alig-self-center">
+                          <tech.icon className="tech-icon" />
+                        </div>
+                        <div className="media-body">
+                          <h6>{tech.name}</h6>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className="media-body">
-                    <h6>{tech.name}</h6>
-                  </div>
-                </div>
-
                 </div>
               </div>
-            </div>
-           </div>
-            ))}
-
-          </div>
-
-    </div>
-
+            </Fade>
+          ))}
+        </div>
+      </div>
     </>
-   
-  )
-}
+  );
+};
 
 export default TechStack;

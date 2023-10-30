158f58e5e3663ef218bfba0bb0b6e62b56d14924
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-30 21:29:39 +0530

    update

--- client/src/componet/pages/Education/Education.jsx
+++ client/src/componet/pages/Education/Education.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import './Education.css';
+import React from "react";
+import "./Education.css";
 import { MdSchool } from "react-icons/md";
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 const Education = () => {
   return (
     <>
-  <div className=" education" id="education">
+      <div className=" education" id="education">
         <h2 className="col-12 mt-3 mb-1 text-center text-uppercase">
           Education Details
         </h2>
         <hr />
         <VerticalTimeline>
-          <VerticalTimelineElement id='date'
+          <VerticalTimelineElement
+            id="date"
             className="vertical-timeline-element--work"
             contentStyle={{ background: "white", color: "black" }}
             contentArrowStyle={{ borderRight: "7px solid  white" }}
@@ -21,9 +25,11 @@ const Education = () => {
             iconStyle={{ background: "#138781", color: "#fff" }}
             icon={<MdSchool />}
           >
-            <h3 className="vertical-timeline-element-title">B.E Computer Engineering</h3>
+            <h3 className="vertical-timeline-element-title">
+              B.E Computer Engineering (8.5 CGPA)
+            </h3>
             <h4 className="vertical-timeline-element-subtitle">
-            Theem college of Engineering,University Of Mumbai,India
+              Theem college of Engineering,University Of Mumbai,India
             </h4>
           </VerticalTimelineElement>
           <VerticalTimelineElement
@@ -34,18 +40,17 @@ const Education = () => {
             iconStyle={{ background: "#138781", color: "#fff" }}
             icon={<MdSchool />}
           >
-            <h3 className="vertical-timeline-element-title">Higher Secondary School</h3>
+            <h3 className="vertical-timeline-element-title">
+              Higher Secondary School
+            </h3>
             <h4 className="vertical-timeline-element-subtitle">
               Mumbai,India.
             </h4>
           </VerticalTimelineElement>
         </VerticalTimeline>
-
-    
       </div>
-      </>
-    
-  )
-}
+    </>
+  );
+};
 
 export default Education;

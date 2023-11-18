e495eeb8353c0664c4053380dfe923572c5ec035
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-18 23:03:18 +0530

    updated

--- client/src/componet/pages/Project/Project.jsx
+++ client/src/componet/pages/Project/Project.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Image from "../../../assets/images/Blog pic.jpg";
 import Download from "../../../assets/images/e-commerce pic.avif";
-import Wheather from "../../../assets/images/wether.jpg";
+import Wheather from "../../../assets/images/Home Page.png";
+import FileShairing from "../../../assets/images/filesharing.png";
+import CRUD from "../../../assets/images/crud images.jpg";
+import Portfolio from "../../../assets/images/Portfolio.png";
 import "./Project.css";
 import Fade from "react-reveal/Fade";
 const Project = () => {
@@ -50,6 +53,7 @@ const Project = () => {
                 </div>
               </div>
             </div>
+
             {/* project Section 2 */}
             <div className="col-md-4">
               <div className="card rounded">
@@ -57,7 +61,7 @@ const Project = () => {
                   <span className="card-notify-badge">
                     Weather web Application
                   </span>
-                  <img src={Wheather} alt="Project 1"></img>
+                  <img src={Wheather} alt="Project 2"></img>
                 </div>
                 <div className="card-image-overly">
                   <span className="card-detail-badge">Html5</span>
@@ -92,7 +96,7 @@ const Project = () => {
                   <span className="card-notify-badge">
                     E-commerce Web Application Using MERN Stack
                   </span>
-                  <img src={Download} alt="Project 1"></img>
+                  <img src={Download} alt="Project 3"></img>
                 </div>
                 <div className="card-image-overly">
                   <span className="card-detail-badge">React.js</span>
@@ -118,6 +122,113 @@ const Project = () => {
                 </div>
               </div>
             </div>
+            {/* File Shairing Project  */}
+            <div className="col-md-4 mt-5">
+              <div className="card rounded">
+                <div className="card-image">
+                  <span className="card-notify-badge">
+                    File Shairing Application
+                  </span>
+                  <img src={FileShairing} alt="Project 4"></img>
+                </div>
+                <div className="card-image-overly">
+                  <span className="card-detail-badge">React.js</span>
+                  <span className="card-detail-badge">Nodejs</span>
+                  <span className="card-detail-badge">MongoDb</span>
+                  <span className="card-detail-badge">Material Ui</span>
+                </div>
+                <div className="card-body">
+                  <div className="ad-title">
+                    <h5 className="text-uppercase">File Shairing</h5>
+                  </div>
+                  <a
+                    className="ad-btn"
+                    href="https://github.com/Touqeer001/File-Sharing-application-Using-MERN-Stack.git"
+                  >
+                    View
+                  </a>
+                  <a
+                    className="ad-btn"
+                    href="https://github.com/Touqeer001/File-Sharing-application-Using-MERN-Stack.git"
+                  >
+                    Github
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* //Project Ending  */}
+
+            {/* Crud operation  */}
+
+            <div className="col-md-4  mt-5">
+              <div className="card rounded">
+                <div className="card-image">
+                  <span className="card-notify-badge">CRUD operations</span>
+                  <img src={CRUD} alt="Project 4"></img>
+                </div>
+                <div className="card-image-overly">
+                  <span className="card-detail-badge">React.js</span>
+                  <span className="card-detail-badge">Nodejs</span>
+                  <span className="card-detail-badge">MongoDb</span>
+                  <span className="card-detail-badge">Material Ui</span>
+                </div>
+                <div className="card-body">
+                  <div className="ad-title">
+                    <h5 className="text-uppercase"> CRUD operations</h5>
+                  </div>
+                  <a
+                    className="ad-btn"
+                    href="https://github.com/Touqeer001/Crud-Operations.git"
+                  >
+                    View
+                  </a>
+                  <a
+                    className="ad-btn"
+                    href="https://github.com/Touqeer001/Crud-Operations.git"
+                  >
+                    Github
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* PRoject End  */}
+
+            {/* -------------------------------------Portfolio------------------------------------------------------ */}
+            <div className="col-md-4  mt-5">
+              <div className="card rounded">
+                <div className="card-image">
+                  <span className="card-notify-badge">Portfolio Website</span>
+                  <img src={Portfolio} alt="Project 4"></img>
+                </div>
+                <div className="card-image-overly">
+                  <span className="card-detail-badge">React.js</span>
+                  <span className="card-detail-badge">Nodejs</span>
+
+                  <span className="card-detail-badge">Bootstrap</span>
+                </div>
+                <div className="card-body">
+                  <div className="ad-title">
+                    <h5 className="text-uppercase"> Portfolio Website</h5>
+                  </div>
+                  <a
+                    className="ad-btn"
+                    href="https://my-portfolio-iota-coral.vercel.app"
+                  >
+                    View
+                  </a>
+                  <a
+                    className="ad-btn"
+                    href="https://github.com/Touqeer001/MyPortfolio.git"
+                  >
+                    Github
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* ----------------------------------------------------------------------------- */}
+
+            {/* <div>Next</div> */}
           </div>
         </div>
       </Fade>

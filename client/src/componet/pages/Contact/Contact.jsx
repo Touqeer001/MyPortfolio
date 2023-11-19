d65576d1a06c470feb3f3ed952ba61ac31bf3b05
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-19 22:56:28 +0530

    update

--- client/src/componet/pages/Contact/Contact.jsx
+++ client/src/componet/pages/Contact/Contact.jsx
@@ -3,53 +3,81 @@ import "./Conatct.css";
 import contact from "../../../assets/images/contact.avif";
 import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
 import Fade from "react-reveal/Fade";
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [msg, setMsg] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: "this msg from Portfolio",
+      to_email: email,
+      message: msg,
+    };
+
+    emailjs
+      .send(
+        "service_iigbxzj",
+        "template_hhx3r0a",
+        templateParams,
+        "aIDExbCzRAjv8ZovD"
+      )
+      .then(
+        (response) => {
+          console.log("Email Sent Successfully!", response);
+          setName("");
+          setEmail("");
+          setMsg("");
+        },
+        (err) => {
+          console.log("FAILED...", err);
+        }
+      );
+  };
+
   return (
     <>
       <Fade bottom>
-        <div className=" contact" id="contact">
+        <div className="contact" id="contact">
           <div className="card card0 border-0">
             <div className="row">
               <div className="col-md-6 col-lg-6 col-xl-6 col-sm-12">
                 <div className="card1">
                   <div className="row border-line">
-                    {/* <LightSpeed> */}
-                    <img src={contact} alt="ocontact" className="image" />
-                    {/* </LightSpeed> */}
+                    <img src={contact} alt="contact" className="image" />
                   </div>
                 </div>
               </div>
               <div className="col-lg-6 col-md-6">
-                {/* <Rotate> */}
                 <div className="card2 d-flex card border-0 px-4 py-5">
                   <div className="row">
                     <div className="row">
                       <h6>
                         Contact With
                         <BsLinkedin color="blue" size={30} className="ms-2" />
-                        
-                        <BsGithub
-                          color="black"
-                          size={30}
-                          className="ms-2"
-                         
-                        />
+                        <BsGithub color="black" size={30} className="ms-2" />
                       </h6>
                     </div>
-
-                    <div className="row px-3 mb-4 ">
+                    <div className="row px-3 mb-4">
                       <div className="line" />
                       <small className="or text-center">OR</small>
                       <div className="line" />
                     </div>
-                    <div className="row px-3 ">
+                    <div className="row px-3">
                       <input
                         type="text"
                         name="name"
                         placeholder="Enter your Name"
                         className="mb-3"
-                        // value={name}
-                        // onChange={(e) => setname(e.target.value)}
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                       />
                     </div>
                     <div className="row px-3">
@@ -58,8 +86,8 @@ const Contact = () => {
                         name="email"
                         placeholder="Enter Your Email Address"
                         className="mb-3"
-                        // value={email}
-                        // onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                       />
                     </div>
                     <div className="row px-3">
@@ -68,17 +96,17 @@ const Contact = () => {
                         name="msg"
                         placeholder="Write your message"
                         className="mb-3"
-                        // value={msg}
-                        // onChange={(e) => setMsg(e.target.value)}
+                        value={msg}
+                        onChange={(e) => setMsg(e.target.value)}
                       />
                     </div>
                     <div className="row px-3">
-                      {/* <button className="button" onClick={handleSubmit}> */}
-                      <button className="button">SEND MESSAGE</button>
+                      <button className="button" onClick={handleSubmit}>
+                        SEND MESSAGE
+                      </button>
                     </div>
                   </div>
                 </div>
-                {/* </Rotate> */}
               </div>
             </div>
           </div>

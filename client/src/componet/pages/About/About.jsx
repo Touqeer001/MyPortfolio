2e1d98c86e082361859fbace4bb0183e165d44c8
Touqeer Ansari <touqeeransari001@gmail.com>
2023-12-16 22:04:26 +0530

    changes

--- client/src/componet/pages/About/About.jsx
+++ client/src/componet/pages/About/About.jsx
@@ -9,11 +9,7 @@ const About = () => {
         <div className="about" id="About">
           <div className="row">
             <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12  about-img">
-              <img
-                // src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
-                src={photo}
-                alt="profile pic"
-              />{" "}
+              <img src={photo} alt="profile pic" />{" "}
             </div>
             <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
               <h1>About me</h1>
